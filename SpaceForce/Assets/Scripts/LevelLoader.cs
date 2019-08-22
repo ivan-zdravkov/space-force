c0047eab183de2fb4b37b888eb95683c2560919f
i-z <IvanZdravkovBG@gmail.com>
2019-08-22 21:23:02 +0300

    Splash, Start Screen, Canvas, Music and SceneLoader.

---
+++ SpaceForce/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        this.currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (this.currentSceneIndex == 0)
+            StartCoroutine(this.WaitOutSplashScreen(seconds: 3));
+    }
+
+    public void LoadSplash()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(this.currentSceneIndex + 1);
+    }
+
+    public IEnumerator WaitOutSplashScreen(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        this.LoadNextScene();
+    }
+}
