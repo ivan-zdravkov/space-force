77a395f58bcc640e4deae1d8875f17635c7d5326
i-z <IvanZdravkovBG@gmail.com>
2019-08-26 01:16:29 +0300

    Projectiles finished.

--- SpaceForce/Assets/Scripts/ShipShooter.cs
+++ SpaceForce/Assets/Scripts/ShipShooter.cs
@@ -4,11 +4,51 @@ using UnityEngine;
 
 public class ShipShooter : MonoBehaviour
 {
-    [SerializeField] [Range(0.1f, 2.0f)] float shootInterval = 2.5f;
+    [SerializeField] [Range(0.1f, 2.0f)] float shootInterval = 1.5f;
     [SerializeField] [Range(0.1f, 0.5f)] float randomFactor = 0.15f;
+    [SerializeField] [Range(0.5f, 5.0f)] float projectileSpeed = 2f;
+    [SerializeField] Projectile projectile;
+    [SerializeField] float damage;
 
-    public void Fire()
+    private float shotCounter;
+
+    void Start()
     {
-        return;
+        this.ResetShoutCounter();
+    }
+
+    void Update()
+    {
+        this.CountDownAndShoot();
+    }
+
+    private void ResetShoutCounter()
+    {
+        this.shotCounter = UnityEngine.Random.Range(this.shootInterval - this.randomFactor, this.shootInterval + this.randomFactor);
+    }
+
+    private void CountDownAndShoot()
+    {
+        this.shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0f)
+        {
+            this.Fire();
+            this.ResetShoutCounter();
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(
+            original: this.projectile.gameObject,
+            position: this.transform.position,
+            rotation: this.projectile.transform.rotation
+        ) as GameObject;
+
+        projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(
+            x: this.projectileSpeed,
+            y: 0
+        );
     }
 }
