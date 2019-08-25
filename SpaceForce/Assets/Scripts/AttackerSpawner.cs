02e45d546907cc727b59cbb4e6da57e2b1ae554f
i-z <IvanZdravkovBG@gmail.com>
2019-08-25 15:07:11 +0300

    Attackers Spawning.

---
+++ SpaceForce/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] [Range(1, 2)] float minSpawn = 1.5f;
+    [SerializeField] [Range(5, 10)] float maxSpawn = 7.5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    IEnumerator Start()
+    {
+        while (this.spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(this.minSpawn, this.maxSpawn));
+
+            this.Spawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+    }
+}
