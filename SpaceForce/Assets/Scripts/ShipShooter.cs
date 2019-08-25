071fd67f1ef1e48a3177e6fd80680cf83cbaf471
i-z <IvanZdravkovBG@gmail.com>
2019-08-25 23:42:26 +0300

    Projectile Animations.

---
+++ SpaceForce/Assets/Scripts/ShipShooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipShooter : MonoBehaviour
+{
+    [SerializeField] [Range(0.1f, 2.0f)] float shootInterval = 2.5f;
+    [SerializeField] [Range(0.1f, 0.5f)] float randomFactor = 0.15f;
+
+    public void Fire()
+    {
+        return;
+    }
+}
