479f8781b8abf623227c143c56b660359de759c7
i-z <IvanZdravkovBG@gmail.com>
2019-12-30 20:50:29 +0200

    Toggle Buttons.

---
+++ SpaceForce/Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        IEnumerable<DefenderButton> buttons = FindObjectsOfType<DefenderButton>();
+
+        foreach(DefenderButton button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(48, 48, 48, 255);
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
