7295027246900ba256c0027de6e95618df74599f
i-z <IvanZdravkovBG@gmail.com>
2019-12-31 00:56:48 +0200

    Resource System.

---
+++ SpaceForce/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    Text starText;
+
+    void Start()
+    {
+        this.starText = GetComponent<Text>();
+
+        this.UpdateDisplay();
+    }
+
+    public void AddStars(int stars)
+    {
+        this.stars += stars;
+
+        this.UpdateDisplay();
+    }
+
+    public void SpendStars(int stars)
+    {
+        if (this.stars >= stars)
+        {
+            this.stars -= stars;
+
+            this.UpdateDisplay();
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        this.starText.text = this.stars.ToString();
+    }
+}
