5de20fc608ef11dfa028eacb50abde081a79149f
i-z <IvanZdravkovBG@gmail.com>
2019-12-31 13:14:38 +0200

    Score And Coin.

--- SpaceForce/Assets/Scripts/Defender.cs
+++ SpaceForce/Assets/Scripts/Defender.cs
@@ -5,4 +5,22 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
+    [SerializeField] bool currencyGain = true;
+    [Range(1, 20)] [SerializeField] int currencyAmount = 10;
+
+    StarDisplay starDisplay;
+
+    public void Start()
+    {
+        this.starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    public void Update()
+    {
+    }
+
+    public void AddStars()
+    {
+        this.starDisplay.AddStars(this.currencyAmount);
+    }
 }
