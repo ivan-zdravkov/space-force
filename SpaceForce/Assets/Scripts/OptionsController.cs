3e15d7ffca0e652db67d0bd24ffdb8a49726e092
i-z <IvanZdravkovBG@gmail.com>
2020-01-01 20:23:08 +0200

    Music Done!

---
+++ SpaceForce/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider difficultySlider;
+
+    [SerializeField] float defaultVolume = 0.5f;
+    [SerializeField] float defaultDifficulty = 0.5f;
+
+    MusicPlayer musicPlayer;
+
+    void Start()
+    {
+        this.volumeSlider.value = PlayerPrefsController.MasterVolume;
+        this.difficultySlider.value = PlayerPrefsController.Difficulty;
+
+        this.musicPlayer = FindObjectOfType<MusicPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.musicPlayer)
+            this.musicPlayer.SetVolume(this.volumeSlider.value);
+    }
+
+    public void ResetToDefault()
+    {
+        this.volumeSlider.value = this.defaultVolume;
+        this.difficultySlider.value = this.defaultDifficulty;
+    }
+
+    public void Save()
+    {
+        PlayerPrefsController.MasterVolume = this.volumeSlider.value;
+        PlayerPrefsController.Difficulty = this.difficultySlider.value;
+    }
+}
---
+++ SpaceForce/Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    const float MIN_VOLUME = 0f;
+    const float MAX_VOLUME = 1f;
+
+    const float MIN_DIFFICULTY = 0f;
+    const float MAX_DIFFICULTY = 1f;
+
+    const string MASTER_VOLUME_KEY = "Master Volume";
+    const string DIFFICULTY_KEY = "Difficulty";
+
+    public static float MasterVolume
+    {
+        get
+        {
+            return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+        }
+        set
+        {
+            if (MIN_VOLUME <= value && value <= MAX_VOLUME)
+                PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, value);
+        }
+    }
+
+    public static float Difficulty
+    {
+        get
+        {
+            return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+        }
+        set
+        {
+            if (MIN_DIFFICULTY <= value && value <= MAX_DIFFICULTY)
+                PlayerPrefs.SetFloat(DIFFICULTY_KEY, value);
+        }
+    }
+}
