56d7d94bf857cede56ac01be9cd7be0206e71e9f
i-z <IvanZdravkovBG@gmail.com>
2020-01-01 23:12:42 +0200

    Buton price and final touches.

--- SpaceForce/Assets/Scripts/DefenderButton.cs
+++ SpaceForce/Assets/Scripts/DefenderButton.cs
@@ -1,10 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab;
+
+    private void Start()
+    {
+        this.LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+
+        if (costText)
+            costText.text = defenderPrefab.GetStarCost().ToString();
+    }
 
     private void OnMouseDown()
     {
