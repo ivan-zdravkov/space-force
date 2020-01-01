3d0223b5316786cfdb4b18b6199c6c04cc434610
i-z <IvanZdravkovBG@gmail.com>
2020-01-01 14:15:41 +0200

    Level Timer.

---
+++ SpaceForce/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] float levelTime = 30;
+
+    Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.slider.value = Time.timeSinceLevelLoad / this.levelTime;
+
+        if (Time.timeSinceLevelLoad >= levelTime)
+        {
+
+        }
+    }
+}
