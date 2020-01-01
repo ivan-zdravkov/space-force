6091359b61a588703f24ca0da6a366d7153b2fe9
i-z <IvanZdravkovBG@gmail.com>
2020-01-01 10:29:16 +0200

    Stop to attack.

--- SpaceForce/Assets/Scripts/Attacker.cs
+++ SpaceForce/Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,21 +7,40 @@ public class Attacker : MonoBehaviour
 {
     float currentSpeed = 0f;
 
+    GameObject currentTarget;
+
     void Start()
     {
     }
 
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime * this.currentSpeed);
+        if (this.currentTarget == null)
+            transform.Translate(Vector2.left * Time.deltaTime * this.currentSpeed);
+        else
+            this.Attack(this.currentTarget);
     }
 
     public void SetMovementSpeed(float speed)
     {
         if (this.currentSpeed != speed)
-        {
-
             this.currentSpeed = speed;
-        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject other = otherCollider.gameObject;
+
+        if (other.GetComponent<Defender>() != null)
+            this.Target(other);
+    }
+
+    public void Target(GameObject target)
+    {
+        this.currentTarget = target;
+    }
+
+    private void Attack(GameObject currentTarget)
+    {
     }
 }
