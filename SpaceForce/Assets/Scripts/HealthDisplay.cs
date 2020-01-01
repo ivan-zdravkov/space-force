a967c325e9059b452f6793b47e7ac322343f8642
i-z <IvanZdravkovBG@gmail.com>
2020-01-01 12:49:53 +0200

    Rotating Health.

---
+++ SpaceForce/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    Text healthText;
+
+    void Start()
+    {
+        this.healthText = GetComponent<Text>();
+
+        this.UpdateDisplay();
+    }
+
+    public void AddHealth(int health)
+    {
+        this.health += health;
+
+        this.UpdateDisplay();
+    }
+
+    public void RemoveHealth(int health)
+    {
+        if (this.health >= health)
+        {
+            this.health -= health;
+
+            this.UpdateDisplay();
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        this.healthText.text = this.health.ToString();
+    }
+}
